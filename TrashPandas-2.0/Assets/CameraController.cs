0558b25c0a8c32b479d010ae9fc37337d13fc01b
Fen Cullen <fen.cullen@gmail.com>
2020-02-07 14:24:17 -0500

    Frictionnn

---
+++ TrashPandas-2.0/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector3 offset;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 cross = Vector3.Cross(Vector3.up, player.transform.right).normalized * offset.z;
+        transform.position = player.transform.position + cross + new Vector3(0, offset.y, 0);
+        transform.LookAt(player.transform.position + new Vector3(0, offset.y, 0));
+    }
+}
