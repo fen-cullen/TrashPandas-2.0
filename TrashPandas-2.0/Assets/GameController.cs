ce8e68c108977b95a35619857ba4d9cf18a0f228
AlexMcCatty <mccatty.a@husky.neu.edu>
2020-02-07 14:21:17 -0500

    Started GameController
    ^

---
+++ TrashPandas-2.0/Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int score;
+    private int remaining;
+    private GameObject[] pickups;
+    private GameObject scoreDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.pickups = GameObject.FindGameObjectsWithTag("pickup");
+        this.remaining = this.pickups.Length;
+        this.scoreDisplay = GameObject.FindGameObjectWithTag("ScoreDisplay");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPickupCollect ()
+    {
+        --this.remaining;
+        ++this.score;
+    }
+}
