bd830fd9ce73c8ee22e6ff43c21844303fc8d815
fen-cullen <54924016+fen-cullen@users.noreply.github.com>
2020-02-11 13:39:14 -0500

    Update CameraController.cs
    This is the correct version

--- TrashPandas-2.0/Assets/Scripts/CameraController.cs
+++ TrashPandas-2.0/Assets/Scripts/CameraController.cs
@@ -1,23 +1,64 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //edit
+    
     public Transform playerBody;
-    public GameObject player;
 
-    public Vector3 offset;
+    float distance;
+    public float distoffset = 10;
 
+    public float rotoffset = 45;
+
+    public float mouseSensitivity = 200f;
+
+
+    // Start is called before the first frame update
     void Start()
     {
+        if (playerBody == null)
+        {
+            playerBody = transform.parent.transform;
+        }
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 
     }
 
-    void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 cross = Vector3.Cross(Vector3.up, player.transform.right).normalized * offset.z;
-        transform.position = player.transform.position + cross + new Vector3(0, offset.y, 0);
-        transform.LookAt(player.transform.position + new Vector3(0, offset.y, 0));
+        float mx = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float my = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //rotate the player left and right so that you can continue to move forward
+        //playerBody.Rotate(Vector3.up * mx);
+
+        //xRotation -= my;
+        //xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        ////rotate the camera up and down like a head
+        //transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        distance = Vector3.Distance(transform.position, playerBody.position);
+
+
+   
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -1 * distoffset, distoffset), transform.position.z);
+
+
+        transform.Translate(Vector3.up * my);
+
+
+
+
+        transform.LookAt(playerBody);
+
+
     }
 }
