c4f7a2f222c06607547b5c0487cab5b1012c83f1
AlexMcCatty <55056536+AlexMcCatty@users.noreply.github.com>
2020-03-20 20:07:05 -0400

    Added a health UI element
    There's a health bar with four hearts, and a script that controlls it.  I made the whole thing a prefab, hopefully it works when you put it in a scene on your end.

---
+++ TrashPandas-2.0/Assets/HealthController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    //float tempTimer = 0;
+
+    public Sprite h0;
+    public Sprite h1;
+    public Sprite h2;
+    public Sprite h3;
+    public Sprite h4;
+
+    public int numHearts = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.numHearts = 4;
+        //this.tempTimer = 0;
+        this.UpdateHearts();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /*
+        if(Time.time - this.tempTimer > 1)
+        {
+            this.TakeDamage(1);
+            this.tempTimer = Time.time;
+        }
+        */
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (this.numHearts > 0)
+        {
+            this.numHearts -= amount;
+            if(this.numHearts < 0)
+            {
+                this.numHearts = 0;
+            }
+            this.UpdateHearts();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (this.numHearts > 0)
+        {
+            this.numHearts += amount;
+            if (this.numHearts > 4)
+            {
+                this.numHearts = 4;
+            }
+            this.UpdateHearts();
+        }
+    }
+
+    private void UpdateHearts()
+    {
+        var temp = this.GetComponent<Image>();
+        switch (this.numHearts)
+        {
+            case (0):
+                temp.sprite = this.h0;
+                print("player died");
+                return;
+            case (1):
+                temp.sprite = this.h1;
+                return;
+            case (2):
+                temp.sprite = this.h2;
+                return;
+            case (3):
+                temp.sprite = this.h3;
+                return;
+            case (4):
+                temp.sprite = this.h4;
+                return;
+            default:
+                temp.sprite = this.h4;
+                return;
+        }
+    }
+}
