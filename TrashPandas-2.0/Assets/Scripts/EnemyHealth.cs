f3e4889438f82ddb7808ccf35c79f8e24bc3c09b
fen-cullen <fen.cullen@gmail.com>
2020-04-03 19:46:49 -0400

    Bee Enemy
    Added bee model and animations, enemyAI script

---
+++ TrashPandas-2.0/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 1;
+    public int currHealth;
+    public AudioClip deathSFX;
+
+    void Start()
+    {
+        currHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amt)
+    {
+        if (currHealth > 0)
+        {
+            currHealth -= amt;
+        }
+
+        if (currHealth <= 0)
+        {
+            //EnemyDies();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Enemy hit?");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            print("Enemy take damage");
+            TakeDamage(1);
+        }
+    }
+}
